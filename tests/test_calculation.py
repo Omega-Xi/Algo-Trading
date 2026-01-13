e21f7c6160a0c5b182d958d4955632f1a84ddbfe
Omega-Xi <akhilantony674@gmail.com>
2026-01-13 18:16:36 +0530

    Tested Indicator Calculations

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,6 +1,7 @@
 from calculations import calculations
 import logging
 from configurations import trading_config
+import pandas as pd
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,6 +14,15 @@ TEST_OPTION_PRICE=600
 TEST_OPTION_DELTA=0.5
 TEST_MARGIN=300000
 TEST_ATR=1
+
+# Example synthetic OHLC data
+data = {
+    "open":  [100, 102, 101, 103, 104, 105, 106, 107],
+    "high":  [103, 104, 103, 105, 106, 107, 108, 109],
+    "low":   [99, 101, 100, 102, 103, 104, 105, 106],
+    "close": [102, 103, 102, 104, 105, 106, 107, 108]
+}
+candle_df = pd.DataFrame(data)
 
 if __name__=="__main__":
     # Check Trigger Price Calculation 
@@ -30,3 +40,12 @@ if __name__=="__main__":
         logging.warning("Margin Too Low")
     else:
         logging.info(f"Quantity Calculation Succesfull :{quantity}")
+
+    # Calculate all indicators
+    df_with_indicators = calculations.calculate_indicators(candle_df)
+    indicator_cols = [
+        "ema12", "ema26", "macd", "signal",
+        "rsi", "atr", "plus_di", "minus_di",
+        "dx", "adx"
+    ]
+    print(df_with_indicators[indicator_cols])
